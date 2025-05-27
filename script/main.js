34b73ff5072d544420b2e689d2e9cb4f4bc09109
harvey puyong <harveyluslos80@gmail.com>
2025-05-27 11:38:02 +0800

    - Implemented functionality to change pages - Reduced animation delay on pages 2, 3, and 4

---
+++ script/main.js
@@ -0,0 +1,51 @@
+const prevArrow = document.querySelector('.arrows-container__arrow.prev');
+const nextArrow =  document.querySelector('.arrows-container__arrow.next');
+
+const sections = document.querySelectorAll('section');
+
+const changeSection = (index) => {
+  sections.forEach(section => {
+    section.style.display = 'none';
+  });
+
+  const displaySection = sections[index]
+
+  if(displaySection.id === 'page-one-section'){
+    displaySection.style.display = 'block';
+    prevArrow.style.opacity = '0';
+    nextArrow.style.opacity = '1'
+  }else if(displaySection.id === 'page-two-section' || displaySection.id === 'page-three-section'){
+    displaySection.style.display = 'flex';
+    prevArrow.style.opacity = '1';
+    nextArrow.style.opacity = '1';
+  }else if(displaySection.id === 'page-four-section'){
+    displaySection.style.display = 'block';
+    prevArrow.style.opacity = '1';
+    nextArrow.style.opacity = '0';
+  }
+}
+
+
+let index = 0;
+const maxIndex = sections.length - 1;
+
+prevArrow.addEventListener('click', () => {
+  index--;
+  if(index <= 0) index = 0;
+
+  changeSection(index);
+});
+
+nextArrow.addEventListener('click', () => {
+  index++;
+  if(index >= maxIndex) index = maxIndex;
+
+  changeSection(index);
+});
+
+
+document.querySelector('.header__contact-btn').
+  addEventListener('click', () => {
+    index = 3;
+    changeSection(index);
+});
