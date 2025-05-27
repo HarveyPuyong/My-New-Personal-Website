580a94232dfabc3ccb205bbf376f95b969329242
harvey puyong <harveyluslos80@gmail.com>
2025-05-27 12:47:20 +0800

    Adjust z-index of mannequin background and main-wrapper on page change
    - Bring mannequin background to the front on page one
    - Move mannequin background behind the main-wrapper on pages 2, 3, and 4

--- script/main.js
+++ script/main.js
@@ -1,5 +1,7 @@
 const prevArrow = document.querySelector('.arrows-container__arrow.prev');
 const nextArrow =  document.querySelector('.arrows-container__arrow.next');
+const maneqquinBackground = document.querySelector('.maneqquin-background');
+const mainWrapper = document.querySelector('.main-wrapper');
 
 const sections = document.querySelectorAll('section');
 
@@ -13,11 +15,15 @@ const changeSection = (index) => {
   if(displaySection.id === 'page-one-section'){
     displaySection.style.display = 'block';
     prevArrow.style.opacity = '0';
-    nextArrow.style.opacity = '1'
+    nextArrow.style.opacity = '1';
+    maneqquinBackground.style.zIndex = '2';
+    mainWrapper.style.zIndex = 'auto';
   }else if(displaySection.id === 'page-two-section' || displaySection.id === 'page-three-section'){
     displaySection.style.display = 'flex';
     prevArrow.style.opacity = '1';
     nextArrow.style.opacity = '1';
+    maneqquinBackground.style.zIndex = '-10';
+    mainWrapper.style.zIndex = '5';
   }else if(displaySection.id === 'page-four-section'){
     displaySection.style.display = 'block';
     prevArrow.style.opacity = '1';
